3b12f1c8df1af195ddfd3a8df538aa46d3f23645
Anger romain <anger.romain@gmail.com>
2021-10-28 16:47:02 +0200

    création et mise en place du PopUp

---
+++ js/popup.js
@@ -0,0 +1,23 @@
+const popUpDisplay = () => {
+  let popUpText = './doc/popUpText.txt';
+  let httpRequest = new XMLHttpRequest();
+  httpRequest.onreadystatechange = function () {
+    if (httpRequest.readyState === XMLHttpRequest.DONE) {
+      // tout va bien, la réponse a été reçue
+      if (httpRequest.status === 200) {
+        console.log(httpRequest.responseText);
+        els.popUp.innerHTML = httpRequest.responseText;
+      } else {
+        // il y a eu un problème avec la requête,
+        console.log("un probleme est intervenue");
+      }
+    }
+  }
+
+  httpRequest.open('GET', popUpText);
+  httpRequest.send();
+}
+
+const popUpClose = () => {
+    els.popUp.classList.add('hidden');
+}
