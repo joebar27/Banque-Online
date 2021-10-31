c13231778d1d7ea1fd88369eba4b21aabc7fbd8f
Anger romain <anger.romain@gmail.com>
2021-10-31 21:19:54 +0100

    toutes les fonctionnalité sont terminer, seul le design est a faire

--- js/statistique.js
+++ js/statistique.js
@@ -1,4 +1,5 @@
 const dispEmprunt = document.querySelector("#dispEmprunt");
+const graphbar = document.querySelector("#graphbar");
 
 const tableEmprunt = () => {
   let statEmprunt = './Data/statistique.json';
@@ -9,15 +10,14 @@ const tableEmprunt = () => {
       // tout va bien, la réponse a été reçue
       if (httpRequest.status === 200) {
         let data = JSON.parse(httpRequest.response);
-        
-        for (let i=0; i < data.length; i++) {
+        // console.log(data[0]);
+        for (let i = 0; i < data[0].length; i++) {
           dispEmprunt.innerHTML += `<tr>
-        <td id="montant" class="text-center align-middle">${data[i].montant}</td>
-        <td id="taux" class="text-center align-middle">${data[i].taux}</td>
-        <td id="duree" class="text-center align-middle">${data[i].duree}</td>
+        <td id="montant" class="text-center align-middle">${data[0][i].montant}</td>
+        <td id="taux" class="text-center align-middle">${data[0][i].taux}</td>
+        <td id="duree" class="text-center align-middle">${data[0][i].duree}</td>
       </tr>`
         }
-        // els.popUp.innerHTML = httpRequest.responseText;
       } else {
         // il y a eu un problème avec la requête,
         console.log("un probleme est intervenue");
@@ -29,4 +29,51 @@ const tableEmprunt = () => {
 
 }
 
+// Fonction de recuperation des données du barregraphe
+const barregraphe = () => {
+  let statEmprunt = './Data/statistique.json';
+  let httpRequest = new XMLHttpRequest();
+
+  httpRequest.onreadystatechange = function () {
+    if (httpRequest.readyState === XMLHttpRequest.DONE) {
+      // tout va bien, la réponse a été reçue
+      if (httpRequest.status === 200) {
+        let data = JSON.parse(httpRequest.response);
+        let data1 = data[1];
+        // console.log("longueur table", data1.length);
+        for (let i = 0; i < data1.length; i++) {
+          //   console.log("data1[i] : ",data1[i]);
+          let points = parseInt(Object.values(data1[i]), 10);
+          //   console.log("points : ", points);
+          let calcHeigth = (((points - 4000) / 3000) * 100).toFixed(2);
+          //   console.log(calcHeigth);
+          updateUsedHeight("bar" + `${i+1}`, calcHeigth);
+        }
+      } else {
+        // il y a eu un problème avec la requête,
+        console.log("un probleme est intervenue");
+      }
+    }
+  }
+  httpRequest.open('GET', statEmprunt);
+  httpRequest.send();
+
+}
+
+// Fonction de changement hauteur des barres graphique
+
+function updateUsedHeight(id, calcHeigth) {
+  //   console.log("id : ", id);
+  //   console.log("calcHeigth", calcHeigth);
+  const div = document.querySelector(`#${id}`);
+  //   console.log("div : ", div);
+  div.style.height = `${calcHeigth}%`;
+}
+
+//################ appel des fonctions ###################
+
+// Tableau des taux d'emprunt
 tableEmprunt();
+
+// Mise en forme du barregraphe selon les donner recuperer
+barregraphe();
